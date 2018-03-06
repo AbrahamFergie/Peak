7ce5587c2dd6b9fb027385ee8605185c522280a0
abrahamfergie <jhnfgie1989@gmail.com>
2018-03-06 11:29:35 -0800

    initial commit with gmail api integration

--- utilities/index.js
+++ utilities/index.js
@@ -1,10 +1,48 @@
-// const fs = require("fs")
+const {google}  = require("googleapis"),
+	  OAuth2 	= google.auth.OAuth2, 
+      Gcalendar = require("google-calendar")
 
- 
+module.exports = {
+	gmail: (accessToken, refreshToken) => {
+		const oauth2Client = new OAuth2(
+			process.env.GOOGLE_CLIENT_ID,
+		    process.env.GOOGLE_CLIENT_SECRET,
+		    process.env.GOOGLE_REDIRECT_URI
+    	)
+    	oauth2Client.setCredentials({
+    		access_token: accessToken,
+    		refresh_token: refreshToken
+    	})
+		const client = google.gmail({
+			version: "v1",
+			auth: oauth2Client
+		}) 
 
-// const photos = fs.readdirSync("./views/images")
-// console.log("page",page)
-// page = photos[Math.floor(Math.random() * photos.length)
-
-const randomImage = Math.floor(Math.random() * 48) + 1
-document.getElementsByTagName("html")[0].style.backgroundImage = "url(../images/image"+ randomImage +".jpg)"
+  		return new Promise((res, rej) => {
+  			client.users.messages.list({
+			    auth: oauth2Client,
+			    userId: 'me',
+			  }, function(err, firstResponse) {
+			    if (err) {
+			      console.log('The API returned an error: ' + err);
+			      return;
+			    }
+			    let messages = firstResponse.data.messages,
+					promises = messages.map((message) => {
+					return new Promise((resolve, reject)=>{
+						client.users.messages.get({
+						'userId': "me",
+						'id': message.id
+						}, function(err, secondResponse){
+							if(err) reject(err)
+							resolve(secondResponse.data.snippet)
+						})	
+					})
+				});		
+				Promise.all(promises).then(data => {
+					return res(data)
+				})
+			});	
+  		})
+	}	
+}
